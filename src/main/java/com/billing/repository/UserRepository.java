0bf14174493fc8f83d37d675bb993563d9c50350
chidrewarkrishna86 <chidrewarkrishna86@gmail.com>
2026-03-11 16:33:02 +0530

    Created UserRepository.java file on 2026-03-11 10:51:25 UTC

---
+++ src/main/java/com/billing/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.billing.repository;
+
+import com.billing.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    List<User> findByActive(Boolean active);
+    List<User> findByRole(User.Role role);
+}
